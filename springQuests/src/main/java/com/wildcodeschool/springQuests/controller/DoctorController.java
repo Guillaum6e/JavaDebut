f163393ac1a6d7db67246e21aff21912f63c0f13
GUILLAUME GROS <guillaume.grosm11@gmail.com>
2023-06-27 10:41:01 +0200

    QuestSpring03

--- springQuests/src/main/java/com/wildcodeschool/springQuests/controller/DoctorController.java
+++ springQuests/src/main/java/com/wildcodeschool/springQuests/controller/DoctorController.java
@@ -1,28 +1,20 @@
 package com.wildcodeschool.springQuests.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.wildcodeschool.models.Doctor;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class DoctorController {
 
-    @GetMapping("/doctor/{id}")
-    @ResponseBody
-    public String doctor(@PathVariable int id) {
-        if (id == 13) {
-            Doctor doctor = new Doctor("Jodie Whittaker", 13);
-            return doctor.getName() + " " + doctor.getNumber();
-        }
-
-        if (id != 13 && id < 13 && id > 0) {
-            return "error 303 : this doctor doesn't exist";
-        }
-
-        return "Impossible de récupérer l'incarnation " + id + " du Docteur";
-
+    @GetMapping("/doctor")
+    public String doctor(
+            @RequestParam(required = false, defaultValue = "0") int number,
+            @RequestParam(required = false, defaultValue = "John Smith") String name, Model model) {
+        model.addAttribute("number", number);
+        model.addAttribute("name", name);
+        System.out.println("number: " + number + " name: " + name);
+        return "doctor";
     }
 }
