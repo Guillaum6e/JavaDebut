35e3f9dd2f9abe0c8be52267d779e37409892e3e
GUILLAUME GROS <guillaume.grosm11@gmail.com>
2023-06-29 15:26:01 +0200

    QuestSpringIoC&DI

--- src/main/java/com/wildcodeschool/wildandwizard/entity/Wizard.java
+++ src/main/java/com/wildcodeschool/wildandwizard/entity/Wizard.java
@@ -1,17 +1,9 @@
 package com.wildcodeschool.wildandwizard.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-
 import java.sql.Date;
 
-@Entity
 public class Wizard {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String firstName;
     private String lastName;
